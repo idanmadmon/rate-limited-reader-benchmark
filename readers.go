c49ca17a925b1b129d41db2efb1bbc6e7f1f09e7
idanmadmon <idan101madmon@gmail.com>
2025-04-24 08:49:07 +0300

    added idanmadmon reader

--- readers.go
+++ readers.go
@@ -1,9 +1,17 @@
 package main
 
-import "io"
+import (
+	"io"
+
+	idanmadmonReader "github.com/idanmadmon/rate-limited-reader"
+)
 
 type ReaderFactory func(io.Reader, int64) io.Reader
 
 func NoLimitReaderFactory(reader io.Reader, limit int64) io.Reader {
 	return reader
+}
+
+func IdanMadmonRateLimitReaderFactory(reader io.Reader, limit int64) io.Reader {
+	return idanmadmonReader.NewRateLimitedReader(reader, limit)
 }
